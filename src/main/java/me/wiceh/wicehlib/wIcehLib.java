e0ba517db53121e7d24c019854f094a6abb6a305
Greezlyyy <87074517+Greezlyyy@users.noreply.github.com>
2024-06-29 17:53:07 +0200

    Initial commit

---
+++ src/main/java/me/wiceh/wicehlib/wIcehLib.java
@@ -0,0 +1,17 @@
+package me.wiceh.wicehlib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class wIcehLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
